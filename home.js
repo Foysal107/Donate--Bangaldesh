4d96b90b205ba0d3a5bae1361c01bf55f7ec3bef
Foysal107 <foysalmiah8443@gmail.com>
2024-09-24 11:26:58 +0600

    enter ammount part done

---
+++ home.js
@@ -0,0 +1,23 @@
+const cardbtn1 = document.getElementById('donate-now-btn1')
+    .addEventListener('click', function () {
+        const cardbutton1 = parseFloat(document.getElementById('donate-ammout1').value);
+        console.log(cardbutton1)
+        
+    });
+
+
+const cardbtntwo = document.getElementById('donate-now-btn2')
+    .addEventListener('click', function () {
+        const cardbuttontwo = parseFloat(document.getElementById('ammount2').value);
+        console.log(cardbuttontwo)
+
+    });
+
+
+    const cardthree = document.getElementById('donate-now-btn3')
+    .addEventListener('click', function () {
+        const cardbuttonthree = parseFloat(document.getElementById('donate-ammount3').value);
+        console.log(cardbuttonthree)
+
+    });
+
